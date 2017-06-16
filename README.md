b2d001ae86e3a8b0e382652a7b0334fa4d05fb0a
helloworldzxy <zxy_bupt@sina.com>
2017-06-16 23:40:04 +0800

    print req content-type

--- README.md
+++ README.md
@@ -3,5 +3,20 @@
 
 这里以git commit的msg为分界线对代码进行说明：
 
-initial commit: 提交请求后只有URL变化了，展示的内容仍然是请求前的内容，相当于浏览器把URL仍然当做`localhost:3000`了。
+1. initial commit: 
 
+提交请求后只有URL变化了，展示的内容仍然是请求前的内容，相当于浏览器把URL仍然当做`localhost:3000`了。
+
+2. POST/GET /url:
+
+服务器发送响应前会检测url，如果是POST表单url变化会显示不同的页面，此时`req.method`是`POST`；如果是直接访问`localhost:3000/url`页面，此时`req.method`是`GET`.
+
+这里用到了两个属性：`req.url`和`req.method`.
+
+`req.url`: 主机名后的所有内容
+`req.method`: HTTP/1.1协议定义的方法：GET（默认）,POST,PUT,DELETE,PATCH。
+
+
+3. print req content-type
+
+不仅响应头有`Content-Type`属性，请求头也有：`req.headers['content-type']`。
--- server.js
+++ server.js
@@ -11,9 +11,16 @@ require('http').createServer(function(req, res){
 			'<p><button>Submit</button></p>',
 			'</form>'
 		].join(''));
-	} else if('/url' == req.url){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.end('You sent a <em>' + req.method + '</em> request');
+	} else if('/url' == req.url && 'POST' == req.method){
+		var body = '';
+		req.on('data', function(chunk){
+			body += chunk;
+		});
+		req.on('end', function(){
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end('<p>Content-Type in req: ' + req.headers['content-type'] + '</p>'
+				+ '<p>Data:</p><pre>' + body + '</pre>');
+		});
+
 	}
-	
 }).listen(3000);
