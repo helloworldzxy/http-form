f333b46f8f617b959c30cd3ef2bb7d226f06f501
helloworldzxy <zxy_bupt@sina.com>
2017-06-16 23:22:48 +0800

    POST/GET /url

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # http-form
 《了不起的Node.js》chap-7 example HTTP 服务器 
+
+这里以git commit的msg为分界线对代码进行说明：
+
+initial commit: 提交请求后只有URL变化了，展示的内容仍然是请求前的内容，相当于浏览器把URL仍然当做`localhost:3000`了。
+
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require('http').createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end([
-		'<form method="POST" action="/url">',
-		'<h1>My Form</h1>',
-		'<fieldset>',
-		'<label>Personal information</label>',
-		'<p>What is your name?</p>',
-		'<input type="text" name="name">',
-		'<p><button>Submit</button></p>',
-		'</form>'
-	].join(''));
+	if('/' == req.url){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end([
+			'<form method="POST" action="/url">',
+			'<h1>My Form</h1>',
+			'<fieldset>',
+			'<label>Personal information</label>',
+			'<p>What is your name?</p>',
+			'<input type="text" name="name">',
+			'<p><button>Submit</button></p>',
+			'</form>'
+		].join(''));
+	} else if('/url' == req.url){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('You sent a <em>' + req.method + '</em> request');
+	}
+	
 }).listen(3000);
